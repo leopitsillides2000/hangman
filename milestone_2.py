f2a3422f4d6a27946e4a1399acb80ce4e25658d5
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-16 22:01:01 +0000

    Checks whether input character is valid

--- milestone_2.py
+++ milestone_2.py
@@ -8,4 +8,12 @@ word = random.choice(word_list)
 
 print(word)
 
-guess = input("Please enter a single letter: ")
+
+while True:
+    guess = input("Please enter a single letter: ")
+    if len(guess) == 1 and guess.isalpha() == True:
+        print("Good guess!")
+        break
+    else:
+        print("Oops! That is not a valid input.")
+
