a411367768ed78320cc2c668093657e87635d537
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-17 08:09:14 +0000

    Rewrites previous code into two functions check_guess and ask_for_input

--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,20 @@
 from milestone_2 import word
 
-while True:
-    guess = input("Please enter a single letter: ")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
+def check_guess(guess):
+    guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.") 
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input():
+    while True:
+        guess = input("Please enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.") 
+    check_guess(guess)
+
+ask_for_input()
+
