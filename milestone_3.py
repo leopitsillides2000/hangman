7676cd32669a9e5331298f5525b8a19b9a7bbf0c
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-17 07:53:38 +0000

    Prints message to suggest whether the guess is on the word or not.

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,13 @@
+from milestone_2 import word
+
 while True:
     guess = input("Please enter a single letter: ")
     if len(guess) == 1 and guess.isalpha() == True:
         break
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print("Invalid letter. Please, enter a single alphabetical character.") 
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry {guess} is not in the word. Try again.")
