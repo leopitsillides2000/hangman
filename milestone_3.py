08f98dba2d8d669cc9987e056c4bda3ee45bef33
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-17 07:56:23 +0000

    Corrects missing comma

--- milestone_3.py
+++ milestone_3.py
@@ -10,4 +10,4 @@ while True:
 if guess in word:
     print(f"Good guess! {guess} is in the word.")
 else:
-    print(f"Sorry {guess} is not in the word. Try again.")
+    print(f"Sorry, {guess} is not in the word. Try again.")
