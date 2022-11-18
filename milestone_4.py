975b8a3d7a9292e4585df55b40a961612498904e
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-18 20:53:35 +0000

    Reduces lives if guess is incorrect and gives corresponding output message to user

--- milestone_4.py
+++ milestone_4.py
@@ -19,6 +19,8 @@ class Hangman:
             self.num_letters -= 1
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
+            self.num_lives -= 1
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         while True:
