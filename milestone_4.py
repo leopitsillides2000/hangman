fb65542ddbc7f73cb7e0dae7dd28b9bcfe0b6fba
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-18 20:22:11 +0000

    Adjusts num_letters initialisation so it gives the number of UNIQUE letters in word

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
         self.word_guessed = ['_' for x in self.word]
-        self.num_letters = len(self.word)
+        self.num_letters = len({x for x in self.word})
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
