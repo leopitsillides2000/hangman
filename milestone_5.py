3bb11541db7797ad58fb0038eaded8245b12db87
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-18 21:53:31 +0000

    Adds num_lives argument to game object.

--- milestone_5.py
+++ milestone_5.py
@@ -36,7 +36,8 @@ class Hangman:
                 break
 
 def play_game(word_list):
-    game = Hangman(word_list)
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
 
     while True:
         if game.num_lives == 0:
