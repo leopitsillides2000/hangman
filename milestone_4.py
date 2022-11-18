49806a9203a70aa67b1a3c65d64d64e0b09f94be
Leo Pitsillides <leo.pitsillides@gmail.com>
2022-11-18 20:14:38 +0000

    Adjusts num_lives initialisation

--- milestone_4.py
+++ milestone_4.py
@@ -1,11 +1,11 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list):
+    def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
         self.word_guessed = ['_' for x in self.word]
         self.num_letters = len(self.word)
-        self.num_lives = 5
+        self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
 
